0d09493454a807c4dcd847dcb59a072d95818126
xiaonitu <1123017460@qq.com>
2019-08-28 19:59:33 +0800

    这是login

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,142 @@
 <template>
-  <div class="index"></div>
+  <el-container class="index">
+    <el-header>
+      <div class="logo">
+        <img src="./../assets/logo.png" alt />
+      </div>
+      <div class="title">
+        <h1>电商后台管理系统</h1>
+      </div>
+      <div class="logout">
+        欢迎光临~
+        <a href="javascript:;" @click="logout" type="text">退出</a>
+      </div>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="user">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="roles">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="rights">
+              <i class="el-icon-menu"></i>
+              <span slot="title">列表权限</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    logout () {
+      this.$confirm('亲，你确定要退出系统吗?', '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message.success('退出成功')
+        localStorage.removeItem('token')
+        this.$router.push('/login')
+      }).catch(() => {
+        this.$message.info('退出取消')
+        // ({
+        //   type: 'info',
+        //   message: '已取消退出'
+        // })
+      })
+    },
+    //  左侧
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+.index {
+  height: 100%;
+  .el-header {
+    background-color: #d8d8d8;
+    display: flex;
+    .logo {
+      flex: 1;
+      width: 180px;
+      img {
+        height: 40px;
+        margin: 0 auto;
+        margin-top: 10px;
+      }
+    }
+    .title {
+      flex: 1;
+      h1 {
+        text-align: center;
+        line-height: 60px;
+      }
+    }
+    .logout {
+      line-height: 60px;
+      text-align: right;
+      a {
+        color: orange;
+        text-decoration: none;
+      }
+    }
+  }
+
+  .el-aside {
+    background-color: #545c64;
+    color: #333;
+
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #ecf0f1;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+}
 </style>
